33967749d5e1630a0567ff53da2baee969d48d01
karinkraken <ka.thuerschmid@gmail.com>
2020-07-25 17:46:35 +0200

    Aufgabe 3

--- src/Geometrics.java
+++ src/Geometrics.java
@@ -1,57 +1,40 @@
 public class Geometrics {
-    private static double x = 10.0;
-    private static double y = 5.0;
-    private static double z = 3.0;
-    private static int x1 = 8;
-    private static int y1 = 7;
-    private static int z1 = 10;
-    private static int x2 = 100;
-    private static int y2 = 57;
-    private static int z2 = 129;
-    private static int x3 = 45;
-    private static int y3 = 89;
-    private static int z3 = 69;
 
     public static void main(String[] args) {
-        // area ... Fläche
-        System.out.println("Square area");
-        System.out.println("x * x = " + x * x);
-        System.out.println("x1 * x1 = " + x1 * x1);
-        System.out.println("x2 * x2 = " + x2 * x2);
-        System.out.println("y * y = " + y * y);
 
-        // perimeter ... Umfang
-        System.out.println("Square perimeter");
-        System.out.println("4 * x = " + 4 * x);
-        System.out.println("4 * x1 = " + 4 * x1);
-        System.out.println("4 * x2 = " + 4 * x2);
-        System.out.println("4 * y = " + 4 * y);
+        System.out.println(rectangle(10.0, 5.0));
+        System.out.println(perimeterSquare(3));
+        System.out.println(squareArea(5));
+        System.out.println(perimeterRectangle(4, 9));
+        System.out.println(zoneSphere(6, 16, 8));
+        System.out.println(cylinderSphere(99));
+        System.out.println(ungula(45, 77));
 
-        System.out.println("Rectangle area");
-        System.out.println("x * y = " + x * y);
-        System.out.println("x1 * y1 = " + x1 * y1);
-        System.out.println("x2 * y2 = " + x2 * y2);
-        System.out.println("x3 * y3 = " + x3 * y3);
 
-        System.out.println("Rectangle perimeter");
-        System.out.println("x + y = " + x + y);
-        System.out.println("x1 + y1 = " + x1 + y1);
-        System.out.println("x2 + y2 = " + x2 + y2);
-        System.out.println("x3 + y3 = " + x3 + y3);
-
-        System.out.println("We can also calculate some volumes");
-        /*
-            z is our height
-            y is our radius
-         */
-        System.out.println("Zone of a sphere");
-        double v = (Math.PI * z * (3 * Math.pow(y, 2) + 3 * Math.pow(x, 2) + Math.pow(z, 2))) / 6;
-        System.out.println(v);
-
-        System.out.println("Sphere with cylinder");
-        System.out.println(Math.PI * Math.pow(z, 3) / 6);
-
-        System.out.println("Ungula");
-        System.out.println((double) (2 * x3 * z3) / 3);
     }
+
+    private static double rectangle (double x, double y){
+        // area ... Fläche
+        return x*y;
+    }
+
+    private static double perimeterSquare (double x){
+        return 4*x;
+    }
+    private static double squareArea (double x){
+        return x*x;
+    }
+    private static double perimeterRectangle (double x, double y){
+        return (x+y)*2;
+    }
+    private static double zoneSphere (double z, double y, double x){
+        return (Math.PI * z * (3 * Math.pow(y, 2) + 3 * Math.pow(x, 2) + Math.pow(z, 2))) / 6;
+    }
+    private static double cylinderSphere (double z){
+        return Math.PI * Math.pow(z, 3) / 6;
+    }
+    private static double ungula (double x3, double z3){
+        return (2 * x3 * z3)/3;
+    }
+
 }
